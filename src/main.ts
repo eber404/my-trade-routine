39488e577af7d1074572a4d901caf8e30c130aea
eber <ecamargo@inclusioncloud.com>
2024-12-26 00:24:08 -0300

    feat: dashboard auto update

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,27 @@ import { NotionTradeRepository } from './repositories/notion-trade-repository'
 
 const tradeRepository = new NotionTradeRepository()
 
-async function main() {
+async function updateDashboard() {
   const trades = await tradeRepository.list()
 
   const dashboard = new Dashboard(trades)
 
+  console.clear()
   console.log('Total sold (BRL):', dashboard.brlSellTotal)
   console.log('Total bought (BRL):', dashboard.brlBuyTotal)
   console.log('Average Buy price (BRL):', dashboard.averageBuyPrice)
   console.log('Average Sell price (BRL):', dashboard.averageSellPrice)
   console.log('Win/Loss (BRL):', dashboard.winLoss)
   console.log('ROI (BRL):', dashboard.roi)
+}
+
+async function main() {
+  try {
+    await updateDashboard()
+    setInterval(updateDashboard, 15_000)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 main()
